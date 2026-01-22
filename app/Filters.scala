a3493b04a80e063701e350cf1b1120a86bfd75b5
James Ward <james@jamesward.com>
2026-01-21 22:05:26 -0700

    cleanup

--- app/Filters.scala
+++
@@ -1,9 +0,0 @@
-import javax.inject.{Inject, Singleton}
-
-import play.api.http.HttpFilters
-import play.filters.gzip.GzipFilter
-
-@Singleton
-class Filters @Inject() (gzipFilter: GzipFilter) extends HttpFilters {
-  override val filters = Seq(gzipFilter)
-}
